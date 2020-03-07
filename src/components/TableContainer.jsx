995af6f36bd05814b137edbc138552dd427c0901
aaron <aaronpiolang@gmail.com>
2020-03-07 13:33:23 -0500

    Add boostrap table

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 import styles from "./TableContainer.module.scss";
-//import Table from "react-bootstrap/Table";
+import Table from "react-bootstrap/Table";
 
 const TableContainer = () => {
-  return <div className={styles.table_container}></div>;
+  return (
+    <div className={styles.table_container}>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default TableContainer;
